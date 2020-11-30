9bfc9bdc5e5c98416003e25795d8110e86f38574
Andres Felipe Duarte Ochoa <aduarte@bancodebogota.com.co>
2020-11-30 16:46:48 -0500

    Ajustes para CircleCI test clientes

--- test_clientes.py
+++ test_clientes.py
@@ -9,5 +9,5 @@ class TestTiendecita:
 
     def test_listarTodos(self):
         expected_output = [{'id': 1, 'nombre': "James Adrian", 'apellido': "Cerati", 'direccion': "Cra 109 Bogota", 'telefono': "3124589635"}]
-        clientes = Clientes()
+        clientes = Clientes().__main__
         assert self.assertContains(clientes.get_byId(1), expected_output)
