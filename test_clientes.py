d9283da1f38d524c766d9768650a4f1f6b90122c
Andres Felipe Duarte Ochoa <aduarte@bancodebogota.com.co>
2020-11-30 15:37:18 -0500

    Ajustes para CircleCI test clientes

--- test_clientes.py
+++ test_clientes.py
@@ -3,10 +3,17 @@ Los recuerdos de ella test
 """
 
 from clientes.principal import *
-
+import json
 
 class TestTiendecita:
 
     def test_listarTodos(self):
+        expected_output = {
+                    'id': 1,
+                    'nombre': "James Adrian",
+                    'apellido': "Cerati",
+                    'direccion': "Cra 109 Bogota",
+                    'telefono': "3124589635"
+                  }
         clientes = Clientes()
-        assert 4 == clientes.get_all()
+        assert json.loads(clientes.get_all()) == expected_output
