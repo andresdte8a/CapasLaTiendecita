3815976571408cc5b615a03efe3ffaf9beb989e1
Andres Felipe Duarte Ochoa <aduarte@bancodebogota.com.co>
2020-11-30 19:22:53 -0500

    Ajustes para CircleCI test clientes

--- test_clientes.py
+++ test_clientes.py
@@ -1,12 +1,11 @@
 """
 Los recuerdos de ella test
 """
-
-from clientes.principal import *
 import requests
+from clientes.principal import *
 
 class TestTiendecita:
 
     def test_listarTodos(self):
         resp = requests.get("http://localhost:8080/clientes/get_all")
-        assert 200 == resp.status_code()
+        assert resp.status_code == 200
